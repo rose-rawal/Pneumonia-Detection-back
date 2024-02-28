031823d019d1e8917a35a968b8c82350ccc91d9a
rose <rooserwl@gmail.com>
2024-02-28 17:16:32 +0545

    history manage payment creating question added

--- model/userSchema.js
+++ model/userSchema.js
@@ -8,5 +8,9 @@ const userSchema = mongoose.Schema({
     type: String,
     require: true,
   },
+  remaining_predict: {
+    type: Number,
+    default: 0,
+  },
 });
 export default mongoose.model("user", userSchema);
